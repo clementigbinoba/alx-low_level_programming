30fe2ea4905f5880597754bb4a6dba644c9f2f64
clementigbinoba <happyclemsy@gmail.com>
2022-12-11 03:50:20 -0800

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*
+*Return: Always 0 (success)
+*
+*/
+
+int main(void)
+{
+	char c, n;
+
+	c = 'a'
+	n = 'A'
+	putchar(c);
+	while (n <= 'A'){
+		putchar(n)
+		n++;
+		}
+		putchar('/n')
+		return (0);
+}
