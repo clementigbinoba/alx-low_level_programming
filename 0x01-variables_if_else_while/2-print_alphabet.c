ba87f75f80846f1515b6d56484cba1b11e75de51
clementigbinoba <happyclemsy@gmail.com>
2022-12-11 00:56:23 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (success)
+*
+*/
+int main(void)
+{
+	char c;
+	c = 'a';
+	while
+		(c <= 'a') {
+			putcher(c);
+		c++;
+		}
+	putcher("\n");
+	return (0);
+}
+
