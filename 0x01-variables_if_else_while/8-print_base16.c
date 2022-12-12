bd5e540e7d33d953b416d0ac7d0ed1ca38c11269
clementigbinoba <happyclemsy@gmail.com>
2022-12-12 12:03:19 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (success)
+*
+*/
+int main(void)
+{
+	char c;
+	int d;
+
+	c = 'a';
+	while
+	(d < 10)
+		{
+			putchar(d + '0');
+			c++;
+		}
+	while
+		(c <= 'a')
+		{
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
