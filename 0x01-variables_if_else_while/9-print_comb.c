0fe9b8ca1367d8b3f683aaca9be3696fb53c5453
clementigbinoba <happyclemsy@gmail.com>
2022-12-12 12:50:57 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (success)
+*
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 0)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
