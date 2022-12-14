8888f857f92fee3a8b543a159273bd2b433a9d9a
clementigbinoba <happyclemsy@gmail.com>
2022-12-13 18:57:45 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _putchar.c - writes the character to c stdout
+ * print_alphabet - print all alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letters++)
+		_putchar('letter');
+
+	print_alphabet('\n');
+}
