c5ba39301efa70d89f952fbc087b39a8e8871baa
clementigbinoba <happyclemsy@gmail.com>
2022-12-15 15:25:03 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_numbers - functions that that printb 0-9
+ * Return: output multiplication
+ */
+void print_numbers(void)
+{
+for (i = 0; i <= 9; 1++)
+_putchar(i + '0');
+_putchar('\n');
+}
