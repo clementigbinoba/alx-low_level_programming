2c4a08795605dca5b656004a9b847da56af273c8
clementigbinoba <happyclemsy@gmail.com>
2022-12-20 19:36:13 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * puts_half - prints half of a string
  * followed by a new line
@@ -16,17 +17,17 @@ void puts_half(char *str)
 	}
 
 	if (len % 2 == 0)
-		
+	{
 		for (i = len / 2; str[i] != '\0'; i++)
 		{
-				_putchar(str[i]);
-				}
+			_putchar(str[i]);
+		}
 	} else if (len % 2)
 	{
 		for (n = (len - 1) / 2; n < len - 1; n++)
 		{
-		_putchar(str[n + 1]);
+			_putchar(str[n + 1]);
 		}
 	}
 	_putchar('\n');
-	}
+}
