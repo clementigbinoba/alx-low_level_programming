703036292a5b2856f1c14510c608195382221f49
clementigbinoba <happyclemsy@gmail.com>
2023-01-08 02:40:56 -0800

    libmy.a

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - short discription, sinhle line
+ * @c: contain value to be compared
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (c > 'a' && c < 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
