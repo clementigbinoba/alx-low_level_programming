fb49ec535d77bee239e50da445e652a279e4ff49
clementigbinoba <happyclemsy@gmail.j.com>
2023-01-29 09:10:38 -0800

    update

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-* main - causes an infinite loop
-*
-* Return: 0
-*/
+ * main - causes an infinite loop
+ *
+ * Return: 0
+ */
 int main(void)
 {
 int i;
@@ -22,4 +22,3 @@ i = 0;
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
-
--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,1 +1,1 @@
-0x03-debugging project
+0x03-debugging
